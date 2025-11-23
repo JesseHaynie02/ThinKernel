bf4c2d295864ef39b25123729d36c74c32d66b2e
Jesse Haynie <jessehaynie02@gmail.com>
2025-11-22 18:42:48 -0700

    Systick delay logic implemented

--- src/systick.c
+++ src/systick.c
@@ -17,7 +17,50 @@ void init_systick()
                     | SysTick_CTRL_ENABLE_Msk;
 }
 
+// TODO: If case should be handled in a kernel task triggered by a semaphore in this interrupt instead of handling it here
+
 void systick_handler()
 {
     systick_ctr++;
+
+    if (delay_list == NULL)
+    {
+        return;
+    }
+
+    if (--delay_list->delay == 0)
+    {
+        Task_t* task = delay_list;
+        delay_list = delay_list->next;
+        task->next = NULL;
+        task->task_state = TASK_STATE_READY;
+
+        Task_t* head = ready_list[task->priority];
+        if (head == NULL)
+        {
+            task->prev = task;
+            task->next = task;
+            ready_list[task->priority] = task;
+        }
+        else
+        {
+            Task_t* tail = head->prev;
+
+            task->next = head;
+            task->prev = tail;
+            tail->next = task;
+            head->prev = task;
+        }
+        ready_bitmap |= (1 << task->priority);
+
+        // Switch to highest priority ready to run task if it changed
+        schedule();
+
+        if (curr_task_ptr != highest_prio_task_ptr)
+        {
+            context_switch();
+        }
+    }
+
+    return;
 }
